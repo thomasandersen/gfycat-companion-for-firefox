3302916323b9397c78ad6e654cff8b3fc4ef523d
Thomas Andersen <thomas@mr-andersen.no>
2014-02-15 16:41:31 +0100

    Video page: add info box

--- src/desktop/chrome/content/js/video-page.js
+++ src/desktop/chrome/content/js/video-page.js
@@ -1,9 +1,9 @@
 var TITLE = "Gfycat Companion: ";
 
-function getImageSrc() {
-  var loc = document.location.href;
-  var src = loc.split("=")[1];
-  return src;
+function getURLParameter(name) {
+  return decodeURI(
+    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+  );
 }
 
 function createPsudoRandomStr(stringLength) {
@@ -23,51 +23,62 @@ function getLoadStatusEl() {
   return document.querySelector("#load-status");
 }
 
-console.log("self", self);
+function getInfoBoxEl() {
+  return document.querySelector("#info-box");
+}
+
+function getBytesSavedEl() {
+  return document.querySelector("#bytes-saved");
+}
+
+function getOriginalImageEl() {
+  return document.querySelector("#original-image");
+}
+
+function getGfyUrlEl() {
+  return document.querySelector("#gfy-url");
+}
 
 function updateTitle(titleStr) {
   document.title = titleStr;
 }
 
-function updateVideo(videoSrc, width) {
-  var videoEl = getVideoEl();
-  var sourceEl = document.createElement("source");
-  videoEl.setAttribute("width", width);
-  videoEl.setAttribute("controls", "true");
-  sourceEl.setAttribute("type", "video/webm");
-  sourceEl.setAttribute("src", videoSrc);
-  videoEl.appendChild(sourceEl);
-  videoEl.addEventListener("loadeddata", function() {
-    getLoadStatusEl().style.display = "none";
-    getVideoEl().style.display = "block";
-    videoEl.play();
-  });
-
-  videoEl.addEventListener("pause", function() {
-    updateTitle(TITLE + videoSrc);
-  });
-
-  videoEl.addEventListener("play", function() {
-    updateTitle("▶ " + TITLE + videoSrc);
-  });
-}
-
-function showImage() {
-  // todo
-}
-
 function onTranscodeSucces(json) {
-  updateVideo(json.webmUrl, json.gifWidth);
+  updateAndShowVideo(json);
 }
 
 function onTranscodeError(json) {
   showImage();
 }
 
+function getBandwidthSavedInMB(json) {
+  var gifSize = json.gifSize;
+  var gfySize = json.gfysize;
+  return (gifSize - gfySize) / 1024 / 1024;
+}
+
+function updateAndShowInfoBox(json) {
+  var infoBox = getInfoBoxEl();
+  var bytesSavedEl = getBytesSavedEl();
+  var originalImageEl = getOriginalImageEl();
+  var gfyUrlEl = getGfyUrlEl();
+  var imageSrc = getURLParameter("s");
+
+  bytesSavedEl.textContent = getBandwidthSavedInMB(json).toPrecision(2) + " MB";
+  
+  originalImageEl.textContent = imageSrc;
+  originalImageEl.setAttribute("href", imageSrc);
+  
+  gfyUrlEl.textContent = "http://gfycat.com/" + json.gfyName;
+  gfyUrlEl.setAttribute("href", "http://gfycat.com/" + json.gfyName);
+
+  infoBox.style.display = "block";
+}
+
 function transcode() {
   var req = new XMLHttpRequest();
   var random = createPsudoRandomStr(6);
-  var imgSrc = getImageSrc();
+  var imgSrc = getURLParameter("s");
   var url = "http://upload.gfycat.com/transcode/" + random + "?fetchUrl=" + imgSrc;
   req.open("GET", url, true);
   req.onload = function(e) {
@@ -82,8 +93,31 @@ function transcode() {
   updateTitle(TITLE + "transcoding gif...");
 }
 
-window.addEventListener("load", transcode);
+function updateAndShowVideo(json) {
+  var videoEl = getVideoEl();
+  var videoSrc = json.webmUrl;
+  var sourceEl = document.createElement("source");
+  videoEl.setAttribute("width", json.gifWidth);
+  videoEl.setAttribute("controls", "true");
+  sourceEl.setAttribute("type", "video/webm");
+  sourceEl.setAttribute("src", videoSrc);
+  videoEl.appendChild(sourceEl);
+  videoEl.addEventListener("loadeddata", function() {
+    getLoadStatusEl().style.display = "none";
+    updateAndShowInfoBox(json);
+    videoEl.style.display = "block";
+    videoEl.play();
+  });
+  videoEl.addEventListener("pause", function() {
+    updateTitle(TITLE + videoSrc);
+  });
+  videoEl.addEventListener("play", function() {
+    updateTitle("▶ " + TITLE + videoSrc);
+  });
+}
 
-/*
-{"gfyname":"BlandInexperiencedDuiker","gfyName":"BlandInexperiencedDuiker","gfysize":133382,"gifSize":1375488,"gifWidth":402,"mp4Url":"http:\/\/zippy.gfycat.com\/BlandInexperiencedDuiker.mp4","webmUrl":"http:\/\/zippy.gfycat.com\/BlandInexperiencedDuiker.webm","frameRate":11,"gifUrl":"http:\/\/fat.gfycat.com\/BlandInexperiencedDuiker.gif"}
-*/
+function showImage() {
+  // todo
+}
+
+window.addEventListener("load", transcode);
